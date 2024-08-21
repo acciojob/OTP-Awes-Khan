bae328089437a9a938383aacabeb5021dba10739
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-21 23:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 //your JS code here. If required.
-const inputs = document.querySelectorAll('.code');
-
-inputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-        if (input.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
+document.querySelectorAll('.code').forEach((input) => {
+    input.addEventListener('focus', function() {
+        this.classList.add('focused');
     });
 
-    input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
-            inputs[index - 1].focus();
-        }
+    input.addEventListener('blur', function() {
+        this.classList.remove('focused');
     });
 });
