f90ff8567650f6472a40ba935a593511dffa1f7e
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-29 00:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-//your JS code here. If required.
-const inputs = document.querySelectorAll('.code');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('.code');
 
-inputs.forEach((input, index) => {
-    input.addEventListener('focus', () => {
-        input.classList.add('focused');
-    });
-    input.addEventListener('blur', () => {
-        input.classList.remove('focused');
-    });
-	input.addEventListener('input', (e) => {
-        if (input.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
-    });
+    inputs.forEach((input, index) => {
+        input.addEventListener('focus', () => {
+            input.classList.add('focused');
+        });
 
-    input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
-            inputs[index - 1].focus();
-        }
+        input.addEventListener('blur', () => {
+            input.classList.remove('focused');
+        });
+
+        input.addEventListener('input', (e) => {
+            if (input.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+                inputs[index - 1].focus();
+            }
+        });
     });
 });
